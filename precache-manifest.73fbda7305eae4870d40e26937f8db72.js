5ccd46722425c3d4f81f2aa16209eebed7c93804
Camilo Fuentes <camilo.fuentest@usach.cl>
2020-07-29 23:25:44 -0400

    Updates

---
+++ precache-manifest.73fbda7305eae4870d40e26937f8db72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f89493d6f82ae104a524ca70538dba7",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "4b78de7410d831798bf1",
+    "url": "/monster-rolodex/static/css/main.68dbd734.chunk.css"
+  },
+  {
+    "revision": "724ff5f7fbcfae15cd6c",
+    "url": "/monster-rolodex/static/js/2.1df26df3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.1df26df3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b78de7410d831798bf1",
+    "url": "/monster-rolodex/static/js/main.bffbea83.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
